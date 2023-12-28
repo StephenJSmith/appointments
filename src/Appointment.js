60fcf390854ff874ebb4e1cfa8da33f96b5ea9fa
Stephen Smith <stephensmith_aus@yahoo.com.au>
2023-12-29 07:39:38 +1100

    Render selected appointment

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Appointment = ({customer}) => (
   <div>{customer.firstName}</div>
 );
 
-export const AppointmentsDayView = ({appointments}) => (
+export const AppointmentsDayView = ({appointments}) => {
+  const [selectedAppointment, setSelectedAppointment] = useState(0);
+
+  return (
   <div id='appointmentsDayView'>
     <ol>
-      {appointments.map(appointment => (
+      {appointments.map((appointment, i) => (
         <li key={appointment.startsAt}>
-          <button type='button'>
+          <button type='button'
+            onClick={() => setSelectedAppointment(i)}
+          >
           {appointmentTimeOfDay(appointment.startsAt)}
           </button>
         </li>
@@ -17,11 +22,11 @@ export const AppointmentsDayView = ({appointments}) => (
       {appointments.length === 0 ? (
       <p>There are no appointments scheduled for today.</p>
       ) : (
-        <Appointment { ...appointments[0] } />
+        <Appointment { ...appointments[selectedAppointment] } />
       )}
     </ol>
   </div>
-);
+);}
 
 const appointmentTimeOfDay = (startsAt) => {
   const [h, m] = new Date(startsAt)
